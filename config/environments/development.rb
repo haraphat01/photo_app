2ed1239fe6addecb291f17baade1533bd08011c1
Akata Arafat <haraphat@gmail.com>
2021-05-14 11:52:39 +0100

    worked on the user authentification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+#to test the email being set up
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
 
   # Show full error reports.
   config.consider_all_requests_local = true
