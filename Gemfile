952eb0c4f77aef410dfc5cd6fbca252889faea0d
Akata Arafat <haraphat@gmail.com>
2021-05-14 22:30:55 +0100

    Added stripe payment to the app

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.5'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.7'
 # Use sqlite3 as the database for Active Record
  gem 'sqlite3', '~> 1.4'
+ gem 'stripe'
 
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
---
+++ app/models/payment.rb
@@ -0,0 +1,20 @@
+class Payment < ApplicationRecord
+    belongs_to :user
+    attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+
+    def self.month_options
+        Date::MONTHNAMES.compact.each_with_index.map {|name, i| ["#{i+1} - #{name }", i + 1]}
+    end
+
+    def self.year_options
+        (Date.today.year..(Date.today.year + 10)).to_a 
+    end
+
+    def process_payment
+        customer = Stripe::Customer.create email:email, card: token
+        Stripe::Charge.create customer: customer.id,
+                            amount: 1000,
+                            description: 'premium',
+                            currency: 'usd'
+    end
+end
